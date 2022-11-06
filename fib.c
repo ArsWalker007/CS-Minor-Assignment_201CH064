516d31f68eeebc5543ef22178168581273684b27
Akshat Raj Singh <99437098+ArsWalker007@users.noreply.github.com>
2022-11-06 22:31:08 +0530

    Add files via upload

---
+++ fib.c
@@ -0,0 +1,51 @@
+#include <stdio.h> //printf and scanf
+#include <stdlib.h>//stderr and atoi
+#include <unistd.h>//wait()
+ 
+int main()
+{
+  int n;
+  printf("please enter the number n\n");
+  scanf("%d",&n);
+  if (n<=0)
+    {
+      printf("Invalid\n");
+      return 1;
+    }
+  pid_t pid = fork();
+  if (pid == 0)
+    {
+      /*child process*/
+      /*here we print out the fibonacci numbers, which I would do like this*/
+      printf("fibonacci-numbers up to %d:\n",n);
+      int *fib_array = (int*)malloc(sizeof(int)*n);
+      int i;
+      for (i=0;i<n;i++)
+    {
+      if (i<=1)
+        fib_array[i] = 1;
+      else
+        fib_array[i] = fib_array[i-1]+fib_array[i-2];
+      printf("%d\n",fib_array[i]);
+    }
+       }
+  if (pid < 0)
+    {
+      printf("error forking child\n");
+      return 1;
+    }
+  if (pid > 0)
+    {
+      /*parent process*/
+      int status;
+      wait(&status);
+      printf("now the child process has exited with the status %d\n",status);
+      return 0;
+    }
+}
+
+
+
+//Credits: Maklolm
+//cboard.cprogramming.com
+//Posted on 11 April 2007
